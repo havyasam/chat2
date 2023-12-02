30647e97063e33c958799a67282d3b54a90e8f1b
havyas <havyasagaltha@gmail.com>
2023-12-02 19:04:30 +0530

    first commit

--- server.js
+++ server.js
@@ -8,14 +8,7 @@ const server = createServer(app);
 const io = new Server(server);
 
 
-app.use(express.static(__dirname + '/public'));
-app.use('/public',express.static(__dirname +'/public'));
-app.use('/js',express.static(__dirname +'/js'));
 
-
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
-});
 
 
 
